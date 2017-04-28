b78897203499556e42a28cba5f422fd2143ad209
Huang, Ying-Tsun <ying-tsunx.huang@intel.com>
2017-04-28 11:26:36 +0800

    Add DebugLibConOut

--- UefiBaseApplication/Include/AutoGen.h
+++ UefiBaseApplication/Include/AutoGen.h
@@ -28,24 +28,28 @@ extern GUID gEfiMdePkgTokenSpaceGuid;
 extern const BOOLEAN _gPcd_FixedAtBuild_PcdVerifyNodeInList;
 #define _PCD_GET_MODE_BOOL_PcdVerifyNodeInList  _gPcd_FixedAtBuild_PcdVerifyNodeInList
 //#define _PCD_SET_MODE_BOOL_PcdVerifyNodeInList  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
 #define _PCD_TOKEN_PcdMaximumLinkedListLength  440U
 extern const UINT32 _gPcd_FixedAtBuild_PcdMaximumLinkedListLength;
 #define _PCD_GET_MODE_32_PcdMaximumLinkedListLength  _gPcd_FixedAtBuild_PcdMaximumLinkedListLength
 //#define _PCD_SET_MODE_32_PcdMaximumLinkedListLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
 #define _PCD_SIZE_PcdMaximumLinkedListLength 4
 #define _PCD_GET_MODE_SIZE_PcdMaximumLinkedListLength _PCD_SIZE_PcdMaximumLinkedListLength
+
 #define _PCD_TOKEN_PcdMaximumAsciiStringLength  441U
 extern const UINT32 _gPcd_FixedAtBuild_PcdMaximumAsciiStringLength;
 #define _PCD_GET_MODE_32_PcdMaximumAsciiStringLength  _gPcd_FixedAtBuild_PcdMaximumAsciiStringLength
 //#define _PCD_SET_MODE_32_PcdMaximumAsciiStringLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
 #define _PCD_SIZE_PcdMaximumAsciiStringLength 4
 #define _PCD_GET_MODE_SIZE_PcdMaximumAsciiStringLength _PCD_SIZE_PcdMaximumAsciiStringLength
+
 #define _PCD_TOKEN_PcdMaximumUnicodeStringLength  442U
 extern const UINT32 _gPcd_FixedAtBuild_PcdMaximumUnicodeStringLength;
 #define _PCD_GET_MODE_32_PcdMaximumUnicodeStringLength  _gPcd_FixedAtBuild_PcdMaximumUnicodeStringLength
 //#define _PCD_SET_MODE_32_PcdMaximumUnicodeStringLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
 #define _PCD_SIZE_PcdMaximumUnicodeStringLength 4
 #define _PCD_GET_MODE_SIZE_PcdMaximumUnicodeStringLength _PCD_SIZE_PcdMaximumUnicodeStringLength
+
 #define _PCD_TOKEN_PcdDebugPropertyMask  443U
 extern const UINT8 _gPcd_FixedAtBuild_PcdDebugPropertyMask;
 #define _PCD_GET_MODE_8_PcdDebugPropertyMask  _gPcd_FixedAtBuild_PcdDebugPropertyMask
@@ -53,6 +57,26 @@ extern const UINT8 _gPcd_FixedAtBuild_PcdDebugPropertyMask;
 #define _PCD_SIZE_PcdDebugPropertyMask 1
 #define _PCD_GET_MODE_SIZE_PcdDebugPropertyMask _PCD_SIZE_PcdDebugPropertyMask
 
+#define _PCD_TOKEN_PcdDebugPrintErrorLevel  4U
+extern const UINT32 _gPcd_FixedAtBuild_PcdDebugPrintErrorLevel;
+#define _PCD_GET_MODE_32_PcdDebugPrintErrorLevel  _gPcd_FixedAtBuild_PcdDebugPrintErrorLevel
+//#define _PCD_SET_MODE_32_PcdDebugPrintErrorLevel  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+#define _PCD_SIZE_PcdDebugPrintErrorLevel 4
+#define _PCD_GET_MODE_SIZE_PcdDebugPrintErrorLevel _PCD_SIZE_PcdDebugPrintErrorLevel
+
+#define _PCD_TOKEN_PcdDebugClearMemoryValue  10U
+extern const UINT8 _gPcd_FixedAtBuild_PcdDebugClearMemoryValue;
+#define _PCD_GET_MODE_8_PcdDebugClearMemoryValue  _gPcd_FixedAtBuild_PcdDebugClearMemoryValue
+//#define _PCD_SET_MODE_8_PcdDebugClearMemoryValue  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+#define _PCD_SIZE_PcdDebugClearMemoryValue 1
+#define _PCD_GET_MODE_SIZE_PcdDebugClearMemoryValue _PCD_SIZE_PcdDebugClearMemoryValue
+
+#define _PCD_TOKEN_PcdFixedDebugPrintErrorLevel  11U
+extern const UINT32 _gPcd_FixedAtBuild_PcdFixedDebugPrintErrorLevel;
+#define _PCD_GET_MODE_32_PcdFixedDebugPrintErrorLevel  _gPcd_FixedAtBuild_PcdFixedDebugPrintErrorLevel
+//#define _PCD_SET_MODE_32_PcdFixedDebugPrintErrorLevel  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+#define _PCD_SIZE_PcdFixedDebugPrintErrorLevel 4
+#define _PCD_GET_MODE_SIZE_PcdFixedDebugPrintErrorLevel _PCD_SIZE_PcdFixedDebugPrintErrorLevel
 
 #ifdef __cplusplus
 }
--- UefiBaseApplication/src/AutoGen.c
+++ UefiBaseApplication/src/AutoGen.c
@@ -7,6 +7,10 @@
 **/
 #include <Uefi.h>
 
+GLOBAL_REMOVE_IF_UNREFERENCED GUID gEfiCallerIdGuid = { 0x1253D572, 0x9309, 0x4CE5, { 0x97, 0x10, 0x0C, 0x1B, 0xB2, 0xFF, 0xF3, 0xA5 } };
+
+GLOBAL_REMOVE_IF_UNREFERENCED CHAR8 *gEfiCallerBaseName = "UefiBaseApplication";
+
 // Definition of PCDs used in libraries
 
 #define _PCD_VALUE_PcdVerifyNodeInList  ((BOOLEAN)0U)
@@ -21,7 +25,7 @@ GLOBAL_REMOVE_IF_UNREFERENCED const UINT32 _gPcd_FixedAtBuild_PcdMaximumAsciiStr
 #define _PCD_VALUE_PcdMaximumUnicodeStringLength  1000000U
 GLOBAL_REMOVE_IF_UNREFERENCED const UINT32 _gPcd_FixedAtBuild_PcdMaximumUnicodeStringLength = _PCD_VALUE_PcdMaximumUnicodeStringLength;
 
-#define _PCD_VALUE_PcdDebugPropertyMask  0x00U
+#define _PCD_VALUE_PcdDebugPropertyMask  0xFFU
 GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 _gPcd_FixedAtBuild_PcdDebugPropertyMask = _PCD_VALUE_PcdDebugPropertyMask;
 
 #define _PCD_VALUE_PcdMaximumDevicePathNodeCount  0U
@@ -44,3 +48,12 @@ GLOBAL_REMOVE_IF_UNREFERENCED const BOOLEAN _gPcd_FixedAtBuild_PcdUgaConsumeSupp
 
 #define _PCD_VALUE_PcdUefiLibMaxPrintBufferSize  320U
 GLOBAL_REMOVE_IF_UNREFERENCED const UINT32 _gPcd_FixedAtBuild_PcdUefiLibMaxPrintBufferSize = _PCD_VALUE_PcdUefiLibMaxPrintBufferSize;
+
+#define _PCD_VALUE_PcdDebugPrintErrorLevel  0x80000040U
+GLOBAL_REMOVE_IF_UNREFERENCED const UINT32 _gPcd_FixedAtBuild_PcdDebugPrintErrorLevel = _PCD_VALUE_PcdDebugPrintErrorLevel;
+
+#define _PCD_VALUE_PcdDebugClearMemoryValue  0xAFU
+GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 _gPcd_FixedAtBuild_PcdDebugClearMemoryValue = _PCD_VALUE_PcdDebugClearMemoryValue;
+
+#define _PCD_VALUE_PcdFixedDebugPrintErrorLevel  0xFFFFFFFFU
+GLOBAL_REMOVE_IF_UNREFERENCED const UINT32 _gPcd_FixedAtBuild_PcdFixedDebugPrintErrorLevel = _PCD_VALUE_PcdFixedDebugPrintErrorLevel;
